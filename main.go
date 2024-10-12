7704069d2a03d76417a81c6c2c212d3c4dcc3f78
ruvice <andrewwee97@gmail.com>
2024-10-12 19:32:36 +0800

    adding chi router

--- main.go
+++ main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
-	}
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("failed to listen to server", err)
-	}
+	r.Get("/hello", basicHandler)
+	http.ListenAndServe(":3000", r)
 }
 
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!"))
+	w.Write([]byte("Hello world2!"))
 }
