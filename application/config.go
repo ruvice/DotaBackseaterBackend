9918a98b6e4ca1c8a971d33fcbce07c1bcfa53cc
ruvice <andrewwee97@gmail.com>
2024-11-19 01:41:17 +0800

    trying github actions

--- application/config.go
+++ application/config.go
@@ -1,6 +1,13 @@
 package application
 
-import "github.com/ruvice/dotabackseaterbackend/wrapper"
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/ruvice/dotabackseaterbackend/wrapper"
+)
 
 type Config struct {
 	RedisAddress  string
@@ -10,10 +17,29 @@ type Config struct {
 }
 
 func LoadConfig() Config {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Failed to get .env")
+	}
+
+	REDIS_ADDR := os.Getenv("REDIS_ADDR")
+	serverPortStr := os.Getenv("SERVER_PORT")
+	if serverPortStr == "" {
+		fmt.Println("SERVER_PORT is not set in the environment")
+	}
+
+	// Convert the string to uint64
+	SERVER_PORT, err := strconv.ParseUint(serverPortStr, 10, 64)
+	if err != nil {
+		fmt.Println("Failed to parse SERVER_PORT: %v", err)
+	}
+	if err != nil {
+		fmt.Println("Invalid PORT value: %v", err)
+	}
 	cfg := Config{
-		// RedisAddress: "localhost:6379",
-		RedisAddress:  "redis:6379",
-		ServerPort:    3000,
+		RedisAddress:  REDIS_ADDR,
+		ServerPort:    SERVER_PORT,
 		TwitchConfig:  LoadTwitchConfig(),
 		MongoDBConfig: LoadMongoDBConfig(),
 	}
