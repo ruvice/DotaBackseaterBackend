f7457dd22cfeabc3055136c28a8edbb40985d6ec
ruvice <andrewwee97@gmail.com>
2025-03-02 04:09:06 +0800

    added starting and stopping vote functionality

--- handler/heroVote.go
+++ handler/heroVote.go
@@ -37,11 +37,39 @@ func (h *Vote) VoteHero(w http.ResponseWriter, r *http.Request) {
 		VoteHeroBody.ChannelID = headerChannelID
 	}
 
-	key := "votesHero:" + VoteHeroBody.ChannelID
-	h.Redis.AddVote(r.Context(), key, VoteHeroBody.HeroID)
-	topVotes, err := h.Redis.GetMostVoted(r.Context(), VoteHeroBody.ChannelID, "Hero", 5)
+	key := "votedHero:" + VoteHeroBody.ChannelID
+	sessionKey := "voteHeroSession:" + VoteHeroBody.ChannelID
+	// Check if key exists - if it does then there's an ongoing vote session
+	hasActiveVoteSession, err := h.hasActiveVoteSession(r.Context(), sessionKey)
+	if !hasActiveVoteSession {
+		log.Println("Received vote with no active vote session")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	if err != nil {
-		log.Println("Failed to retrieve most voted heroes")
+		log.Println("Error retrieving current active vote session: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// Handling vote relation
+	voteRelationKey := "voteRelation:" + VoteHeroBody.ChannelID + ":" + VoteHeroBody.TwitchID
+	hasVoteRelation := h.Redis.GetHeroVoteRelation(r.Context(), voteRelationKey)
+	if hasVoteRelation {
+		log.Println("User already voted")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	voteError := h.Redis.AddHeroVoteRelation(r.Context(), voteRelationKey)
+	if voteError != nil {
+		log.Println(voteError)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// Adding actual vote
+	h.Redis.AddVote(r.Context(), key, VoteHeroBody.HeroID)
+	topVotes, err := h.Redis.GetMostVoted(r.Context(), key, 5)
+	if err != nil {
+		log.Println("Failed to retrieve most voted heroes", err)
 	}
 	jsonData, err := json.Marshal(topVotes)
 	if err != nil {
@@ -56,7 +84,6 @@ func (h *Vote) VoteHero(w http.ResponseWriter, r *http.Request) {
 func (h *Vote) StartHeroVote(w http.ResponseWriter, r *http.Request) {
 	log.Println("Starting Vote")
 	channelID := r.Header.Get("Channel-Id")
-	log.Println(channelID)
 	if channelID == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -68,7 +95,6 @@ func (h *Vote) StartHeroVote(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	key := fmt.Sprintf("votesHero:%s", channelID)
 	durationInt, err := strconv.Atoi(VoteStartBody.Duration)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -83,25 +109,33 @@ func (h *Vote) StartHeroVote(w http.ResponseWriter, r *http.Request) {
 	duration := time.Duration(durationInt) * time.Second
 
 	// Set expiration on the key
-	err = h.Redis.SetExpiry(r.Context(), key, duration)
+	sessionKey := "voteHeroSession:" + channelID
+	log.Println("Adding sessionkey", sessionKey)
+	err = h.Redis.Client.SetEx(r.Context(), sessionKey, "started", duration).Err()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		log.Println("Failed to setup vote session with expiry")
 		return
 	}
+	hasActiveVoteSession, err := h.Redis.KeyExists(r.Context(), sessionKey)
+	if err != nil {
+		log.Println("Eror getting active vote session", err)
+		return
+	}
+	log.Println(hasActiveVoteSession)
 	time.AfterFunc(duration, func() {
 		h.stopVote(context.Background(), channelID)
 	})
 
 	fmt.Printf("Voting session started for channel %s, expires in %v seconds.\n", channelID, duration.Seconds())
 	w.WriteHeader(http.StatusOK)
+	SSEPushChannel <- SSEPushRequest{SSEMessage: SSEMessage{EventType: "voteSession", Data: "started"}, ChannelID: channelID}
 }
 
 func (h *Vote) StopHeroVote(w http.ResponseWriter, r *http.Request) {
 	log.Println("Stopping Vote")
 	channelID := r.Header.Get("Channel-Id")
 	if channelID == "" {
-		VoteHeroBody.ChannelID = channelID
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -110,28 +144,34 @@ func (h *Vote) StopHeroVote(w http.ResponseWriter, r *http.Request) {
 
 func (h *Vote) stopVote(ctx context.Context, channelID string) {
 	log.Println("Stopping Vote")
+	sessionKey := "voteHeroSession:" + channelID
+	err := h.Redis.Client.Del(ctx, sessionKey).Err()
+	if err != nil {
+		log.Printf("Failed to delete session key %s: %v\n", sessionKey, err)
+	}
 
-	topVotes, err := h.Redis.GetMostVoted(ctx, channelID, "Hero", 5)
+	key := "votedHero:" + channelID
+	topVotes, err := h.Redis.GetMostVoted(ctx, key, 5)
 	if err != nil {
 		log.Println("Failed to retrieve most voted heroes")
 	}
 	message := ""
 	for id, votes := range topVotes {
 		votedHero := h.Redis.GetHeroByID(ctx, id)
-		message += fmt.Sprintf("%s: %d votes", votedHero.Name, votes)
+		message += fmt.Sprintf("%s: %d votes\n", votedHero.Name, votes)
 	}
 
 	var twitchMessage = wrapper.TwitchMessage{
 		Message:   message,
-		ChannelID: VoteHeroBody.ChannelID,
+		ChannelID: channelID,
 	}
-	timeout := h.Redis.GetTwitchMessageAPITimeout(ctx, VoteHeroBody.ChannelID)
+	timeout := h.Redis.GetTwitchMessageAPITimeout(ctx, channelID)
 	if timeout < 0 {
 		err := h.TwitchWrapper.SendMessage(twitchMessage)
 		if vErr := new(dbsError.VoteError); errors.As(err, &vErr) {
 			if vErr.Code == dbsError.CodeTwitchMessageTooManyRequests {
 				log.Println("Too many requests error: %w", vErr)
-				h.handleVoteMessageTooManyRequests(ctx, VoteHeroBody.ChannelID)
+				h.handleVoteMessageTooManyRequests(ctx, channelID)
 			}
 		}
 	}
@@ -141,5 +181,83 @@ func (h *Vote) stopVote(ctx context.Context, channelID string) {
 		return
 	}
 
-	SSEPushChannel <- SSEPushRequest{SSEMessage: SSEMessage{EventType: "votedHero", Data: jsonData}, ChannelID: VoteHeroBody.ChannelID}
+	err = h.Redis.Client.Del(ctx, key).Err()
+	if err != nil {
+		log.Printf("Failed to delete key %s: %v\n", key, err)
+	}
+
+	voteRelationKey := "voteRelation:" + VoteHeroBody.ChannelID + ":"
+	err = h.Redis.ClearHeroVoteRelation(context.Background(), voteRelationKey)
+	if err != nil {
+		log.Println("Failed to clear hero vote relation", err)
+	}
+
+	SSEPushChannel <- SSEPushRequest{SSEMessage: SSEMessage{EventType: "votedHero", Data: string(jsonData)}, ChannelID: channelID}
+	SSEPushChannel <- SSEPushRequest{SSEMessage: SSEMessage{EventType: "voteSession", Data: "stopped"}, ChannelID: channelID}
+}
+
+func (h *Vote) GetExtensionHeroVoteStatus(w http.ResponseWriter, r *http.Request) {
+	channelID := r.Header.Get("Channel-Id")
+	twitchID := r.Header.Get("Twitch-Id")
+	if channelID == "" {
+		log.Println("Missing channelID in URL")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if twitchID == "" {
+		log.Println("Missing twitchID in URL")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	key := "votedHero:" + channelID
+	sessionKey := "voteHeroSession:" + channelID
+	// Check if key exists - if it does then there's an ongoing vote sessio
+	hasActiveVoteSession, err := h.hasActiveVoteSession(r.Context(), sessionKey)
+	if err != nil {
+		log.Println("Error retrieving current active vote session: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if !hasActiveVoteSession {
+		log.Println("No active vote session", sessionKey)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	topVotes, err := h.Redis.GetMostVoted(r.Context(), key, 5)
+	if err != nil {
+		log.Println("Failed to retrieve most voted heroes")
+	}
+	// jsonData, err := json.Marshal(topVotes)
+	// if err != nil {
+	// 	log.Println("Error encoding JSON:", err)
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	return
+	// }
+	var response struct {
+		HeroVoteMap map[string]int `json:"hero_vote_map"`
+		HasVoted    bool           `json:"has_voted"`
+	}
+
+	voteRelationKey := "voteRelation:" + channelID + ":" + twitchID
+	hasVoted := h.Redis.GetHeroVoteRelation(r.Context(), voteRelationKey)
+
+	response.HeroVoteMap = topVotes
+	response.HasVoted = hasVoted
+	data, err := json.Marshal(response)
+	log.Println("ANDREW WTF", response, data)
+	if err != nil {
+		log.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func (h *Vote) hasActiveVoteSession(ctx context.Context, key string) (bool, error) {
+	hasActiveVoteSession, err := h.Redis.KeyExists(ctx, key)
+	if err != nil {
+		return false, err
+	}
+	return hasActiveVoteSession, nil
 }
--- repository/redisRepo/timeoutsRepo.go
+++ repository/redisRepo/timeoutsRepo.go
@@ -8,35 +8,91 @@ import (
 	"github.com/ruvice/dotabackseaterbackend/utils/dbsError"
 )
 
-func (r *RedisRepo) AddVoteRelation(ctx context.Context, channelID string, twitchID string) error {
+func (r *RedisRepo) AddHeroVoteRelation(ctx context.Context, voteRelationKey string) error {
 	// Set the key with a 30-second expiration
-	key := channelID + ":" + twitchID
 	value := time.Now()
-	err := r.Client.Set(ctx, key, value, VoteRelationTTL*time.Second).Err()
+	err := r.Client.Set(ctx, voteRelationKey, value, 0).Err()
 	if err != nil {
-		log.Println("failed to write to Redis with expiry: %w", err)
-		voteError := dbsError.NewVoteError("AddVoteRelation", dbsError.CodeVoteRelationCreationError, "Unable to add Vote Relation", err)
+		log.Println("Failed to set hero vote relation: %w", err)
+		voteError := dbsError.NewVoteError("AddHeroVoteRelation", dbsError.CodeVoteRelationCreationError, "Unable to add Hero Vote Relation", err)
 		return voteError
 	}
 
-	log.Printf("Successfully set key '%s' with value '%s' and 30-second expiry\n", key, value)
+	log.Printf("Successfully set key '%s' with value '%s' and 30-second expiry\n", voteRelationKey, value)
 	return nil
 }
 
-func (r *RedisRepo) GetVoteRelationTTL(ctx context.Context, channelID string, twitchID string) int64 {
+func (r *RedisRepo) GetHeroVoteRelation(ctx context.Context, voteRelationKey string) bool {
 	// Retrieve the value for the given key
-	key := channelID + ":" + twitchID
-	ttl, err := r.Client.TTL(ctx, key).Result()
+	_, err := r.Client.Get(ctx, voteRelationKey).Result()
+	if err != nil {
+		log.Println("Error getting vote relation: ", err)
+		return false
+	}
+	// Return the TTL in seconds
+	return true
+}
+
+func (r *RedisRepo) ClearHeroVoteRelation(ctx context.Context, prefix string) error {
+	var cursor uint64 = 0
+	var keys []string
+	var err error
+	for {
+		var result []string
+		result, cursor, err = r.Client.Scan(ctx, cursor, prefix+"*", 100).Result()
+		if err != nil {
+			return err
+		}
+
+		keys = append(keys, result...)
+
+		if cursor == 0 { // No more keys to scan
+			break
+		}
+	}
+
+	if len(keys) == 0 {
+		log.Println("No matching keys found.")
+		return nil
+	}
+
+	// Delete all matching keys
+	err = r.Client.Del(ctx, keys...).Err()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Deleted %d keys with prefix '%s'\n", len(keys), prefix)
+	return nil
+}
+
+func (r *RedisRepo) AddItemVoteRelation(ctx context.Context, voteRelationKey string) error {
+	// Set the key with a 30-second expiration
+	value := time.Now()
+	err := r.Client.Set(ctx, voteRelationKey, value, VoteRelationTTL*time.Second).Err()
+	if err != nil {
+		log.Println("failed to write to Redis with expiry: %w", err)
+		voteError := dbsError.NewVoteError("AddItemVoteRelation", dbsError.CodeVoteRelationCreationError, "Unable to add Item Vote Relation", err)
+		return voteError
+	}
+
+	log.Printf("Successfully set key '%s' with value '%s' and 30-second expiry\n", voteRelationKey, value)
+	return nil
+}
+
+func (r *RedisRepo) GetItemVoteRelationTTL(ctx context.Context, voteRelationKey string) int64 {
+	// Retrieve the value for the given key
+	ttl, err := r.Client.TTL(ctx, voteRelationKey).Result()
 	if err != nil {
 		log.Println("Unable to get TTL for vote relation: ", err)
 		return 0
 	}
 	// Check the TTL value
 	if ttl == -1 {
-		log.Printf("Key '%s' does not have an expiry set\n", key)
+		log.Printf("Key '%s' does not have an expiry set\n", voteRelationKey)
 		return -1
 	} else if ttl == -2 {
-		log.Printf("Key '%s' does not exist\n", key)
+		log.Printf("Key '%s' does not exist\n", voteRelationKey)
 		return -2
 	}
 
